095f32cd7ef7bc1f6f26e30d06e9253aeda58a11
ketan <code.ketan@gmail.com>
2017-06-02 16:47:26 +0530

    Added routes

--- basic-express-website/routes/index.js
+++ basic-express-website/routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Home' });
+});
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About Us' });
+});
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'Contact Us' });
+});
+
+router.post('/contact/send', (req, res, next) => {
+	//res.json(req.body);
 });
 
 module.exports = router;
