caa3020e9a67b1e9c50606581dfe0186e704c4c7
ketan <code.ketan@gmail.com>
2017-06-02 17:25:35 +0530

    added nodemailer support

--- basic-express-website/routes/index.js
+++ basic-express-website/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Home' });
 });
@@ -14,6 +14,29 @@ router.get('/contact', (req, res, next) => {
 
 router.post('/contact/send', (req, res, next) => {
 	//res.json(req.body);
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: '', //Put username & password.
+			pass: ''
+		}
+	});
+	var mailOptions = {
+		from: '', //Supply all the following details
+		to: '',
+		subject: '',
+		text: 'New submission: ' +req.body.name+ '' +req.body.email+ '' +req.body.message,
+		html: '<p>New submission</p><p>' +req.body.name+'</p><p>' +req.body.email+ '</p><p>' +req.body.message+'</p>'
+	};
+	transporter.sendMail(mailOptions, (err, info) => {
+		if(err) {
+			console.log(err);
+			res.redirect('/contact');
+		} else {
+			console.log("Successful");
+			res.redirect('/');
+		}
+	});
 });
 
 module.exports = router;
